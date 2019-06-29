d93b64de687de02259acd2bd5656933fda56fd36
Harish Sriram <harish11109026@gmail.com>
2019-06-29 11:10:15 +0530

    29th June, 2019 - commit for list implementation

---
+++ session_04.py
@@ -0,0 +1,36 @@
+'''
+List
+'''
+#
+# var_list = []
+# var_list1 = list()
+# var_dict = {}
+# var_dict1 = dict()
+# var_set = {}
+# var_set1 = set()
+# var_tuple = ('',)
+# var_list.append(6)
+# print(var_list[1::2])
+# var_list.insert(3, 3.5)
+# inserts value at particular index
+# print(var_list)
+# x = len(var_list)
+# print(x)
+# print(var_list.count(3.5))
+# counts the number of times the value appears in the list
+# var_list.pop()
+# deletes the index value which is provided
+# print(var_list)
+# for i in var_list:
+#     print(i)
+# enumerate returns value along with index
+# for index, i in enumerate(var_list):
+#     print(index, i)
+# demonstrate nested list
+# var_list1 = ['test1', 'test2', ['test3', 'test4', ['test5']]]
+# print(var_list1[2][2][0])
+
+# Assignment question - 06/29/2019
+var_list = [1, 2, 3, 4, 5]
+var_list2 = ['test1', 'test2', ['test3', ['test4']], 'test5', ['test6', ['test7'], 'test8', ['test9', 'catch_me']]]
+print(var_list2[4][3][1])
