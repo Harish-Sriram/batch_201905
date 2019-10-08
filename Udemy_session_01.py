dadf6f170ef2ba23b25d26435eef49136bb23e34
Harish Sriram <harish11109026@gmail.com>
2019-10-08 12:33:21 +0530

    Udemy session 1 - 8th oct, 2019

---
+++ Udemy_session_01.py
@@ -0,0 +1,26 @@
+"""
+Numbers in Python
+Variable name
+"""
+# Adding two numbers
+print(2+2)
+
+# performing MOD operation
+print(7 % 4)
+
+# performing POWER OF operation
+print(2**3)
+
+# variable names with type() and id()
+var_a = 10
+var_b = 10
+print(id(var_a), type(var_a))
+print(id(var_b), type(var_b))
+
+"""
+Assigning variables
+"""
+a = 10
+b = type(a), id(a)
+print(id(b))
+
